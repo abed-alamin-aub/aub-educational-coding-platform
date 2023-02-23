fd3e8a6cb8d5172bdaa109344804bdf94d4bd23f
Abed Al Mottaleb Al Amin <60525094+abed-alamin-aub@users.noreply.github.com>
2023-02-23 21:40:15 +0200

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { createBrowserHistory } from "history";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-widgets/dist/css/react-widgets.css';
+import "semantic-ui-css/semantic.min.css";
+import "../src/app/common/assets/css/style.css";
+import App from "./app/layout/App";
+import ScrollToTop from "./app/layout/ScrollToTop";
+import * as serviceWorker from "./serviceWorker";
+
+export const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history = {history}>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </Router>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
